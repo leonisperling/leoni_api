c919de1b8abcc3cd7e28d71d853630d974c86b27
Leoni Sperling <leoni.sperling@gmail.com>
2020-03-19 19:15:23 +0100

    add radius restrictions

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,16 +1,17 @@
 class LocationsController < ApplicationController
   def create
+    return unless within_radius?
+
     @vehicle = Vehicle.find_by(uuid: params[:vehicle_id])
     @location = Location.new(
       lat: params[:lat],
       lng: params[:lng],
-      at: params[:at],
+      at: params[:at]
     )
     @location.vehicle = @vehicle
     if @location.save
       render json: nil, status: 204
     else
-      p @location.errors
       render json: @location.errors, status: 403
     end
   end
@@ -18,15 +19,14 @@ class LocationsController < ApplicationController
   private
 
   def within_radius?
-    office_coordinates = [52.53, 13.403]
-    radius = 3_500
+    office_coordinates = [[52.53, 13.403], [51.50, 12.4]]
+    radius = 3.5
+    distance = Geocoder::Calculations.distance_between(office_coordinates, coordinates)
 
+    distance < radius
   end
 
   def coordinates
     [params[:lat], params[:lng]]
   end
-
-
-
 end
